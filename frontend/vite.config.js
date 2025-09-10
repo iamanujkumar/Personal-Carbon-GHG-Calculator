13fdeea7722a63835ee53124cd16156210351bbd
iamanujkumar <anujkumar73889@gmail.com>
2025-09-10 20:19:58 +0530

    render url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://personal-carbon-ghg-calculator.onrender.com',
         changeOrigin: true,
         secure: false,
       },
